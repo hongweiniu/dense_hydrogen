b7b09e203081a7a7a4a2d058b5961e81ec787254
Hongwei “Ricky” Niu (牛宏伟) <niuhw@illinois.edu>
2020-08-13 19:13:53 -0500

    加入cal_bond_list

--- dhtool/bond.py
+++ dhtool/bond.py
@@ -3,7 +3,6 @@
 ----------
 和bond有关的一些函数
 '''
-import pandas as pd
 import numpy as np
 from ase import io
 
@@ -24,6 +23,19 @@ def cal_bond_list(f_extxyz, frame, bond_length):
     ----------
     无
     '''
-    atoms = io.read(f_extxyz, index=':', format='extxyz')
-    print(atoms)
-
+    atoms = io.read(f_extxyz, index='%d' % frame, format='extxyz')
+    distances = atoms.get_all_distances(mic=True)
+    bond_list_init = list()
+    for i in range(len(distances)):
+        nearest_atom = np.argsort(distances[i])[1]
+        if atoms.get_distance(i, nearest_atom, mic=True) > bond_length:
+            continue
+        bond_list_init += [[i, nearest_atom]]
+    # print(bond_list_init)
+    # print(len(bond_list_init))
+    bond_list = list()
+    for i in range(len(bond_list_init)):
+        if [bond_list_init[i][0], bond_list_init[i][1]] in bond_list_init and [bond_list_init[i][1], bond_list_init[i][0]] in bond_list_init and bond_list_init[i][0] < bond_list_init[i][1]:
+            bond_list += [[bond_list_init[i][0], bond_list_init[i][1]]]
+    # print(bond_list)
+    return bond_list
