0bb6b3766f37ef3de83a87e15a278d8fbd0fc13d
Hongwei “Ricky” Niu (牛宏伟) <niuhw@illinois.edu>
2020-08-20 17:45:45 -0500

    加入move_bonded_atoms函数

--- dhtool/bond.py
+++ dhtool/bond.py
@@ -4,6 +4,7 @@
 和bond有关的一些函数
 '''
 import numpy as np
+from ase import Atoms
 
 
 def cal_bond_list(atoms, bond_length):
@@ -14,13 +15,12 @@ def cal_bond_list(atoms, bond_length):
 
     参数
     ----------
-    atoms: ASE中的atoms对象
-    frame: which frame of extxyz to calculate bond list
+    atoms: ASE中的atoms对象(单帧)
     bond_length: 最大键长
 
     返回值
     ----------
-    无
+    bond_list: numpy array
     '''
     distances = atoms.get_all_distances(mic=True)
     bond_list_init = list()
@@ -37,3 +37,37 @@ def cal_bond_list(atoms, bond_length):
             bond_list += [[bond_list_init[i][0], bond_list_init[i][1]]]
     # print(bond_list)
     return bond_list
+
+
+def move_bonded_atoms(atoms, bond_list, flag):
+    '''
+    功能
+    ----------
+    将atoms(单帧)中边界处成键的原子移动到盒子上方或者下方
+
+    参数
+    ----------
+    atoms: ASE中的atoms对象(单帧)
+    bond_list: bond list
+    flag: 移动到上方或者下方的标志。'u'或者'd'
+
+    返回值
+    ----------
+    atoms_moved: ASE中的atoms对象(单帧)
+    '''
+    lz = atoms.get_cell()[2][2]
+    positions = atoms.get_positions()
+    for i in bond_list:
+        if np.abs(positions[i[1]][2] - positions[i[2]][2]) > lz/2.0:
+            if flag == 'u':
+                if positions[i[1]][2] > positions[i[2]][2]:
+                    positions[i[2]][2] += lz
+                else:
+                    positions[i[1]][2] += lz
+            if flag == 'd':
+                if positions[i[1]][2] > positions[i[2]][2]:
+                    positions[i[1]][2] -= lz
+                else:
+                    positions[i[2]][2] -= lz
+    atoms_moved = Atoms(symbols=atoms.get_chemical_symbols(), positions=positions, cell=atoms.get_cell(), pbc=atoms.get_pbc())
+    return atoms_moved
